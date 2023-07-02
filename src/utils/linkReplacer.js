0da47e95817ee4c307009ce8efa063c89af84f81
Everett Southwick <everett@southwick.dev>
2023-07-02 01:00:36 -0400

    far more specific regex matching

--- src/utils/linkReplacer.js
+++ src/utils/linkReplacer.js
@@ -1,30 +1,24 @@
 module.exports = function replaceLink(message) {
-  // Regex patterns to match URLs
-  let twitterPattern = /(http[s]?:\/\/(?:www\.)?twitter\.([a-zA-Z0-9-.]+))/g;
-  let instagramPattern = /(http[s]?:\/\/(?:www\.)?instagram\.([a-zA-Z0-9-.]+))/g;
-  let tiktokPattern = /(http[s]?:\/\/(?:www\.)?tiktok\.([a-zA-Z0-9-.]+))/g;
-  
-  let wwwPattern = /(www\.)/g;
+	// Regex patterns to match URLs
+  const instagramPattern = /(http[s]?:\/\/(?:www\.)?instagram\.[a-zA-Z0-9-]+\/(?:[a-zA-Z0-9_.]+\/(?:stories|live|p|reel)\/)?[a-zA-Z0-9_-]+(?:\?.*)?)/g;
+  const tiktokPattern = /(http[s]?:\/\/(?:www\.)?tiktok\.[a-zA-Z0-9-]+\/@(?:[a-zA-Z0-9_.]+\/video\/)?[a-zA-Z0-9_.]+(?:\/t\/ZT[a-zA-Z0-9_]+\/)?(?:\?.*)?)/g;
+  const twitterPattern = /(http[s]?:\/\/(?:www\.|m\.|mobile\.)?twitter\.[a-zA-Z0-9-]+\/(?:i\/status|([a-zA-Z0-9_]+)\/status)\/([0-9]+)(\?[a-zA-Z0-9_=&-]*)?)/g;
 
-  let newMessage = message;
+	let newMessage = message;
 
   // Check if the message contains a URL of the old domain and replace it
-  if (twitterPattern.test(message)) {
-    newMessage = newMessage.replace(twitterPattern, (url) => url.replace("twitter.", "vxtwitter."));
-  }
+	if (instagramPattern.test(message)) {
+		newMessage = newMessage.replace(instagramPattern, (url) => url.replace('instagram.', 'ddinstagram.'));
+	}
+
+	if (tiktokPattern.test(message)) {
+		newMessage = newMessage.replace(tiktokPattern, (url) => url.replace('tiktok.', 'vxtiktok.'));
+	}
+
   
-  if (instagramPattern.test(message)) {
-    newMessage = newMessage.replace(instagramPattern, (url) => url.replace("instagram.", "ddinstagram."));
-  }
+	if (twitterPattern.test(message)) {
+		newMessage = newMessage.replace(twitterPattern, (url) => url.replace('twitter.', 'vxtwitter.'));
+	}
 
-  if (tiktokPattern.test(message)) {
-    newMessage = newMessage.replace(tiktokPattern, (url) => url.replace("tiktok.", "vxtiktok."));
-  }
-
-  // After domain replacement, remove 'www.' from any URL
-  if (wwwPattern.test(newMessage)) {
-    newMessage = newMessage.replace(wwwPattern, "");
-  }
-
-  return newMessage;
-}
+	return newMessage;
+};
