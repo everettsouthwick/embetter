3b013fdadc5929bf61ca47aa4358527d49cdbc9b
Everett Southwick <everett@southwick.dev>
2023-07-01 23:27:14 -0400

    remove www. from new url

--- src/utils/linkReplacer.js
+++ src/utils/linkReplacer.js
@@ -1,23 +1,30 @@
 module.exports = function replaceLink(message) {
-	// Regex patterns to match URLs
-	const twitterPattern = /(http[s]?:\/\/(?:www\.)?twitter\.([a-zA-Z0-9-.]+))/g;
-	const instagramPattern = /(http[s]?:\/\/(?:www\.)?instagram\.([a-zA-Z0-9-.]+))/g;
-	const tiktokPattern = /(http[s]?:\/\/(?:www\.)?tiktok\.([a-zA-Z0-9-.]+))/g;
+  // Regex patterns to match URLs
+  let twitterPattern = /(http[s]?:\/\/(?:www\.)?twitter\.([a-zA-Z0-9-.]+))/g;
+  let instagramPattern = /(http[s]?:\/\/(?:www\.)?instagram\.([a-zA-Z0-9-.]+))/g;
+  let tiktokPattern = /(http[s]?:\/\/(?:www\.)?tiktok\.([a-zA-Z0-9-.]+))/g;
+  
+  let wwwPattern = /(www\.)/g;
 
-	let newMessage = message;
+  let newMessage = message;
 
-	// Check if the message contains a URL of the old domain and replace it
-	if (twitterPattern.test(message)) {
-		newMessage = newMessage.replace(twitterPattern, (url) => url.replace('twitter.', 'vxtwitter.'));
-	}
+  // Check if the message contains a URL of the old domain and replace it
+  if (twitterPattern.test(message)) {
+    newMessage = newMessage.replace(twitterPattern, (url) => url.replace("twitter.", "vxtwitter."));
+  }
+  
+  if (instagramPattern.test(message)) {
+    newMessage = newMessage.replace(instagramPattern, (url) => url.replace("instagram.", "ddinstagram."));
+  }
 
-	if (instagramPattern.test(message)) {
-		newMessage = newMessage.replace(instagramPattern, (url) => url.replace('instagram.', 'ddinstagram.'));
-	}
+  if (tiktokPattern.test(message)) {
+    newMessage = newMessage.replace(tiktokPattern, (url) => url.replace("tiktok.", "vxtiktok."));
+  }
 
-	if (tiktokPattern.test(message)) {
-		newMessage = newMessage.replace(tiktokPattern, (url) => url.replace('tiktok.', 'vxtiktok.'));
-	}
+  // After domain replacement, remove 'www.' from any URL
+  if (wwwPattern.test(newMessage)) {
+    newMessage = newMessage.replace(wwwPattern, "");
+  }
 
-	return newMessage;
-};
+  return newMessage;
+}
