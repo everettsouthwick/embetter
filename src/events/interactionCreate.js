fdac4a8d676bcaa3339378b91753285d93812a13
Everett Southwick <everett@southwick.dev>
2023-07-04 02:50:55 -0400

    action row buttons for manual and reply mode

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,21 +3,21 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand() && !interaction.isContextMenuCommand()) return;
+		if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+			const command = interaction.client.commands.get(interaction.commandName);
 
-		const command = interaction.client.commands.get(interaction.commandName);
+			if (!command) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
 
-		if (!command) {
-			console.error(`No command matching ${interaction.commandName} was found.`);
-			return;
-		}
-
-		try {
-			await command.execute(interaction);
-		}
-		catch (error) {
-			console.error(`Error executing ${interaction.commandName}`);
-			console.error(error);
+			try {
+				await command.execute(interaction);
+			}
+			catch (error) {
+				console.error(`Error executing ${interaction.commandName}`);
+				console.error(error);
+			}
 		}
 	},
 };
--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,28 +1,11 @@
 const { Events } = require('discord.js');
-const replaceLink = require('../utils/linkReplacer.js');
-const EmbedMode = require('../utils/embedMode.js');
-const { getGuildMode } = require('../utils/db.js');
-
+const handleLinkMessage = require('../utils/handleLinkMessage.js');
 
 module.exports = {
 	name: Events.MessageCreate,
-	execute(message) {
+	async execute(message) {
 		if (!message.author.bot) {
-			const { fullMessage, links } = replaceLink(message.content);
-			if (links.length > 0) {
-				getGuildMode(message.guildId, (mode) => {
-					if (mode == EmbedMode.REPLACE) {
-						message.delete();
-						message.channel.send(`${message.author}: ${fullMessage}`);
-					}
-					else if (mode == EmbedMode.REPLY) {
-						message.reply(links.join('\n'));
-					}
-					else {
-						// Do nothing
-					}
-				});
-			}
+            handleLinkMessage(message);
 		}
 	},
 };
