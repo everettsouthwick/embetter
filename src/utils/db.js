5bdd841915060ebd09ce1402de8bf5ead33d1a92
Everett Southwick <everett@southwick.dev>
2023-07-05 09:31:47 -0400

    wip mongodb

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,51 +1,34 @@
-const sqlite3 = require('sqlite3').verbose();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { mongoDbUri } = require('../../config.json')
 
-const db = new sqlite3.Database('./embetter.db', (err) => {
-	if (err) {
-		return console.error(err.message);
+const client = new MongoClient(mongoDbUri, {
+	serverApi: {
+	  version: ServerApiVersion.v1,
+	  strict: true,
+	  deprecationErrors: true,
 	}
-	console.log('Connected to the SQlite database.');
 });
 
-db.run(`CREATE TABLE IF NOT EXISTS guild_modes (
-    guild_id TEXT PRIMARY KEY,
-    mode INTEGER
-)`, (err) => {
-	if (err) {
-		return console.error(err.message);
-	}
-	console.log('Guild modes table created.');
-});
+let db;
 
-function setGuildMode(guild_id, mode) {
-	return new Promise((resolve, reject) => {
-		db.run('REPLACE INTO guild_modes (guild_id, mode) VALUES (?, ?)', [guild_id, mode], (err) => {
-			if (err) {
-				console.error(err.message);
-				reject(err);
-			}
-			else {
-				console.log(`Mode ${mode} set for guild ${guild_id}.`);
-				resolve();
-			}
-		});
-	});
+async function run() {
+	try {
+	  await client.connect();
+      db = client.db("embetter");
+	} catch (err) {
+		console.error(err);
+	}
+}
+run().catch(console.dir);
+
+async function setGuildMode(guild_id, mode) {
+  const collection = db.collection('guild_modes');
+  return collection.updateOne({ _id: guild_id }, { $set: { mode: mode } }, { upsert: true });
 }
 
-function getGuildMode(guild_id) {
-	return new Promise((resolve, reject) => {
-		db.get('SELECT mode FROM guild_modes WHERE guild_id = ?', [guild_id], (err, row) => {
-			if (err) {
-				console.error(err.message);
-				reject(err);
-			}
-			else {
-				// If no results were found, row will be `undefined`, and mode will be `null`.
-				const mode = row ? row.mode : null;
-				resolve(mode);
-			}
-		});
-	});
+async function getGuildMode(guild_id) {
+  const collection = db.collection('guild_modes');
+  return collection.findOne({ _id: guild_id });
 }
 
 module.exports = { setGuildMode, getGuildMode };
