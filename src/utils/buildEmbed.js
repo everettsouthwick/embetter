e4277538a81bb13227f523e060495182ec6179af
Everett Southwick <everett@southwick.dev>
2023-07-12 19:18:30 -0400

    fix merge

--- src/utils/buildEmbed.js
+++ src/utils/buildEmbed.js
@@ -3,10 +3,13 @@ const { fetchWebsiteDetails } = require('./fetchWebsite.js');
 
 async function buildEmbed(platform, originalUrl, newUrl) {
 	try {
+		const embeds = [];
+		const addedImageUrls = new Set();
+
 		const data = await fetchWebsiteDetails(originalUrl);
 		const base = new URL(data.ogUrl || data.twitterUrl || data.requestUrl || originalUrl);
 
-		if (data.favicon?.includes('.svg')) {
+		if (data.favicon?.includes('.ico')) {
 			data.favicon = null;
 		}
 
@@ -22,23 +25,48 @@ async function buildEmbed(platform, originalUrl, newUrl) {
 			data.twitterImage[0].url = base.origin + data.twitterImage[0].url;
 		}
 
-		const test = data.favicon || platform.embed?.author?.iconURL || null;
-		console.log(test);
+		if (data.ogImage?.length > 1 || data.twitterImage?.length > 1) {
+			data.thumbnail = null;
+		}
 
 		const embed = new EmbedBuilder()
 			.setAuthor({
-				name: data.ogSiteName || data.alAndroidAppName || data.alIphoneAppName || data.twitterAppNameGooglePlay || base.host || platform.embed?.name || null,
+				name: data.ogSiteName || data.alAndroidAppName || data.alIphoneAppName || data.twitterAppNameGooglePlay || base.host || platform.name || null,
 				url: data.ogUrl || data.twitterUrl || data.requestUrl || originalUrl || null,
-				iconURL: data.favicon || platform.embed?.author?.iconURL || null,
+				iconURL: data.favicon || platform.author?.iconURL || null,
 			})
-			.setTitle(data.ogTitle || data.twitterTitle || platform.embed?.name || null)
+			.setTitle(data.ogTitle || data.twitterTitle || platform.name || null)
 			.setURL(newUrl || null)
 			.setDescription(data.ogDescription || data.twitterDescription || null)
 			.setImage(data.ogImage?.length > 0 ? data.ogImage[0]?.url : null || data.twitterImage?.length > 0 ? data.twitterImage[0]?.url : null || null)
 			.setThumbnail(data.thumbnail || null)
-			.setColor(platform.embed?.color || [48, 209, 88])
+			.setColor('#0a84ff')
 			.setTimestamp(new Date(data.ogDate || data.articleModifiedTime || data.articlePublishedTime || Date.now()) || null);
-		return embed;
+
+		if (embed.image && embed.image.url) {
+			addedImageUrls.add(embed.image.url);
+		}
+
+		embeds.push(embed);
+
+		const imagesArray = data.ogImage || data.twitterImage || null;
+
+		if (imagesArray?.length > 1) {
+			for (let i = 1; i < imagesArray.length; i++) {
+				const image = imagesArray[i];
+
+				if (image.url && !addedImageUrls.has(image.url)) {
+					addedImageUrls.add(image.url);
+
+					embeds.push(new EmbedBuilder()
+						.setURL(newUrl || null)
+						.setImage(image.url || null),
+					);
+				}
+			}
+		}
+
+		return embeds;
 	}
 	catch (error) {
 		console.error('Error building embed:', error);
--- src/utils/handleLink.js
+++ src/utils/handleLink.js
@@ -27,6 +27,7 @@ function getPlatform(message, guildProfile) {
 	return null;
 }
 
+
 function replaceLink(message, platform) {
 	const originalUrl = message.match(platform.pattern)[0];
 	let newMessage = message;
@@ -40,33 +41,30 @@ function replaceLink(message, platform) {
 	return { newMessage, originalUrl, newUrl };
 }
 
-async function handleEmbed(platform, originalUrl, newUrl) {
-	let embed = null;
+async function handleEmbeds(platform, originalUrl, newUrl) {
+	let embeds = [];
 	if (platform.embed) {
 		try {
-			embed = await buildEmbed(platform, originalUrl, newUrl);
+			embeds = await buildEmbed(platform, originalUrl, newUrl);
 		}
 		catch (error) {
 			console.error('Error building embed for', platform.name, ':', error);
 		}
 	}
-	return embed;
+	return embeds;
 }
 
 async function processLink(message, guildProfile) {
 	let newMessage = message;
 	const links = [];
-	const embeds = [];
+	let embeds = [];
 
 	const platform = getPlatform(message, guildProfile);
 	if (platform) {
 		const replacementResult = replaceLink(message, platform);
 		newMessage = replacementResult.newMessage;
-		const embed = await handleEmbed(platform, replacementResult.originalUrl, replacementResult.newUrl);
-		if (embed) {
-			embeds.push(embed);
-		}
-		else {
+		embeds = await handleEmbeds(platform, replacementResult.originalUrl, replacementResult.newUrl);
+		if (!embeds.length === 0) {
 			links.push(replacementResult.newUrl);
 		}
 	}
@@ -76,7 +74,7 @@ async function processLink(message, guildProfile) {
 
 async function processArchive(link) {
 	const links = [];
-	const embeds = [];
+	let embeds = [];
 
 	if (!isValidUrl(link)) {
 		return { links: links, embeds: embeds };
@@ -90,11 +88,9 @@ async function processArchive(link) {
 	};
 
 	const newUrl = platform.replacement(strippedLink);
-	let embed;
 
 	try {
-		embed = await buildEmbed(platform, strippedLink, newUrl);
-		embeds.push(embed);
+		embeds = await buildEmbed(platform, strippedLink, newUrl);
 	}
 	catch (error) {
 		console.error('Error building embed for', platform.name, ':', error);
