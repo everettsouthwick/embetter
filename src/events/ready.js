bd9cb3039d06b7404d87b93f508acf52a5175687
Everett Southwick <everett@southwick.dev>
2023-07-12 09:36:50 -0400

    remove getting public ip

--- src/events/ready.js
+++ src/events/ready.js
@@ -7,12 +7,5 @@ module.exports = {
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		console.log(`Active in ${client.guilds.cache.size} servers`);
-
-		try {
-			const res = await axios.get('https://api.ipify.org?format=json');
-			console.log(`Public IP: ${res.data.ip}`);
-		} catch (err) {
-			console.error('Error getting public IP:', err);
-		}
 	},
 };
