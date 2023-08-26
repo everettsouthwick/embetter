9423447ca47295e515df36f182ca83b396a88c78
Everett Southwick <everett@southwick.dev>
2023-08-26 14:19:39 -0400

    fix: don't delete archive non-embeds

--- src/utils/handleLinkResponse.js
+++ src/utils/handleLinkResponse.js
@@ -4,182 +4,79 @@ const { Mode } = require('../models/Mode.js');
 
 async function sendReplaceModeMessage(message, fullMessage, embeds) {
 	await message.delete();
-	if (embeds.length > 0) {
-		await message.channel.send({ content: `${message.author}: ${fullMessage}`, embeds: embeds });
-	}
-	else {
-		await message.channel.send({ content: `${message.author}: ${fullMessage}` });
-	}
+	const content = { content: `${message.author}: ${fullMessage}`, embeds: embeds.length ? embeds : undefined };
+	await message.channel.send(content);
 }
 
 async function sendReplyModeMessage(messageOrInteraction, links, embeds) {
-	const isInteraction = messageOrInteraction.commandId ? true : false;
+	const isInteraction = Boolean(messageOrInteraction.commandId);
 	const userId = isInteraction ? messageOrInteraction.user.id : messageOrInteraction.author.id;
-
-	const confirm = new ButtonBuilder().setCustomId('keep').setLabel('Keep').setStyle(ButtonStyle.Primary);
-	const cancel = new ButtonBuilder().setCustomId('delete').setLabel('Delete').setStyle(ButtonStyle.Danger);
-	const row = new ActionRowBuilder().addComponents(confirm, cancel);
-
-	const options = embeds.length > 0 ? { embeds, components: [row] } : { content: links.join('\n') };
+	const components = [new ActionRowBuilder().addComponents(
+		new ButtonBuilder().setCustomId('keep').setLabel('Keep').setStyle(ButtonStyle.Primary),
+		new ButtonBuilder().setCustomId('delete').setLabel('Delete').setStyle(ButtonStyle.Danger),
+	)];
+	const options = { content: links.join('\n'), embeds: embeds.length ? embeds : undefined, components };
 	const response = await messageOrInteraction.reply(options);
 
-	if (!isInteraction && !(await checkForEmbed(response))) return await response.delete();
-
-	if (!isInteraction) await response.edit({ components: [row] });
-
-	const collector = await response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 180000 });
-
-	collector.on('collect', async (i) => {
-		if (i.customId !== 'keep' && i.customId !== 'delete') return;
-
-		if (i.customId === 'keep') {
-			if (!isInteraction) await messageOrInteraction.suppressEmbeds(true);
-			await collector.stop();
-			return await response.edit({ components: [] });
-		}
-
-		if (i.user.id !== userId && !i.member.permissions.has('MANAGE_MESSAGES')) {
-			return await i.reply({ content: 'You are not authorized to delete this message.', ephemeral: true });
-		}
-
-		await collector.stop();
-		await response.delete();
-	});
-
-	collector.on('end', async () => {
-		try {
-			await response.edit({ components: [] });
-		}
-		catch {
-			// Do nothing
-		}
-	});
-}
-
-
-async function sendAskModeMessage(message, links, embeds) {
-	const confirm = new ButtonBuilder()
-		.setCustomId('yes')
-		.setLabel('Yes')
-		.setStyle(ButtonStyle.Primary);
-
-	const cancel = new ButtonBuilder()
-		.setCustomId('no')
-		.setLabel('No')
-		.setStyle(ButtonStyle.Secondary);
-
-	const row = new ActionRowBuilder()
-		.addComponents(confirm, cancel);
-
-	const response = await message.reply({ content: 'Would you like to embed this link?', components: [row] });
+	if (!isInteraction && !(await checkForEmbed(response, embeds))) return await response.delete();
+	if (!isInteraction) await response.edit({ components });
 
 	const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 180000 });
-
 	collector.on('collect', async (i) => {
-		if (i.customId === 'yes') {
-			await response.delete();
-			await sendReplyModeMessage(message, links, embeds);
-		}
-		else if (i.customId === 'no') {
-			if (i.user.id === message.author.id) {
-				await response.delete();
-			}
-			else {
-				await i.reply({ content: 'Only the original author can choose not to embed.', ephemeral: true });
-			}
-		}
+		if (i.customId === 'keep') return collector.stop() && await response.edit({ components: [] });
+		if (i.user.id !== userId && !i.member.permissions.has('MANAGE_MESSAGES')) return await i.reply({ content: 'You are not authorized to delete this message.', ephemeral: true });
+		await collector.stop() && await response.delete();
 	});
+	collector.on('end', async () => await response.edit({ components: [] }).catch(() => {}));
+}
 
-	collector.on('end', async () => {
-		try {
-			await response.delete();
-		}
-		catch {
-			// Do nothing
-		}
+async function sendAskModeMessage(message, links, embeds) {
+	const components = [new ActionRowBuilder().addComponents(
+		new ButtonBuilder().setCustomId('yes').setLabel('Yes').setStyle(ButtonStyle.Primary),
+		new ButtonBuilder().setCustomId('no').setLabel('No').setStyle(ButtonStyle.Secondary),
+	)];
+	const response = await message.reply({ content: 'Would you like to embed this link?', components });
+
+	const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 180000 });
+	collector.on('collect', async (i) => {
+		if (i.customId === 'yes') return await response.delete() && await sendReplyModeMessage(message, links, embeds);
+		if (i.user.id !== message.author.id && !i.member.permissions.has('MANAGE_MESSAGES')) return await i.reply({ content: 'You are not authorized to choose not to embed', ephemeral: true });
+		await response.delete();
 	});
+	collector.on('end', async () => await response.delete().catch(() => {}));
 }
 
 async function checkForEmbed(message) {
+	if (message.content.includes('archive.today')) return Promise.resolve(true);
 	const startTime = Date.now();
-
-	return new Promise((resolve, reject) => {
-		const embedCheck = setInterval(() => {
-			if (message.embeds.length > 0) {
-				clearInterval(embedCheck);
-				resolve(true);
-			}
-			else if (Date.now() - startTime > 10000) {
-				clearInterval(embedCheck);
-				reject(false);
-			}
+	return new Promise((resolve) => {
+		const check = setInterval(() => {
+			if (message.embeds.length) return clearInterval(check) && resolve(true);
+			if (Date.now() - startTime > 10000) return clearInterval(check) && resolve(false);
 		}, 200);
 	});
 }
 
-
-async function handleContextMenuLink(interaction) {
-	const { links, embeds } = await processLink(interaction.targetMessage.content);
-	if (links.length > 0 || embeds.length > 0) {
-		await sendReplyModeMessage(interaction, links, embeds);
-	}
-	else {
-		interaction.reply({ content: 'No valid link was found.', ephemeral: true });
-	}
-}
-
-async function handleContextMenuArchive(interaction) {
-	const { links, embeds } = await processArchive(interaction.targetMessage.content);
-	if (embeds.length > 0) {
-		await sendReplyModeMessage(interaction, links, embeds);
-	}
-	else {
-		interaction.reply({ content: 'No valid link was found.', ephemeral: true });
-	}
-}
-
-async function handleSlashCommandLink(interaction) {
-	const { links, embeds } = await processLink(interaction.options.getString('link'));
-	if (links.length > 0 || embeds.length > 0) {
-		await sendReplyModeMessage(interaction, links, embeds);
-	}
-	else {
-		interaction.reply({ content: 'No valid link was found.', ephemeral: true });
-	}
-}
-
-async function handleSlashCommandArchive(interaction) {
-	const { links, embeds } = await processArchive(interaction.options.getString('link'));
-	if (embeds.length > 0) {
-		await sendReplyModeMessage(interaction, links, embeds);
-	}
-	else {
-		interaction.reply({ content: 'No valid link was found.', ephemeral: true });
-	}
+async function handleInteraction(interaction, processFn) {
+	const { links, embeds } = await processFn(interaction.options.getString('link'));
+	if (!links.length && !embeds.length) return interaction.reply({ content: 'No valid link was found.', ephemeral: true });
+	await sendReplyModeMessage(interaction, links, embeds);
 }
 
 async function handleMessageLink(message) {
-	const guildProfileService = message.client.guildProfileService;
-	const guildProfile = await guildProfileService.getGuildProfile(message.guild.id);
-
+	const guildProfile = await message.client.guildProfileService.getGuildProfile(message.guild.id);
 	const { fullMessage, links, embeds } = await processLink(message.content, guildProfile);
-	if (links.length > 0 || embeds.length > 0) {
-		if (guildProfile?.mode == Mode.REPLACE) {
-			await sendReplaceModeMessage(message, fullMessage, embeds);
-		}
-		else if (guildProfile?.mode == Mode.REPLY) {
-			await sendReplyModeMessage(message, links, embeds);
-		}
-		else if (guildProfile?.mode == Mode.ASK) {
-			await sendAskModeMessage(message, links, embeds);
-		}
-	}
+
+	if (!links.length && !embeds.length) return;
+	if (guildProfile?.mode === Mode.REPLACE) return await sendReplaceModeMessage(message, fullMessage, embeds);
+	if (guildProfile?.mode === Mode.REPLY) return await sendReplyModeMessage(message, links, embeds);
+	if (guildProfile?.mode === Mode.ASK) return await sendAskModeMessage(message, links, embeds);
 }
 
 module.exports = {
-	handleContextMenuLink,
-	handleContextMenuArchive,
-	handleSlashCommandLink,
-	handleSlashCommandArchive,
+	handleContextMenuLink: (interaction) => handleInteraction(interaction, processLink),
+	handleContextMenuArchive: (interaction) => handleInteraction(interaction, processArchive),
+	handleSlashCommandLink: (interaction) => handleInteraction(interaction, processLink),
+	handleSlashCommandArchive: (interaction) => handleInteraction(interaction, processArchive),
 	handleMessageLink,
 };
